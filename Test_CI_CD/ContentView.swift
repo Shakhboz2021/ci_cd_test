feb6e1b1f6c5983de5f577abcb3acd4b012bd87e
Muhammad <muhammad.tohirov@fido.uz>
2024-06-06 15:41:42 +0500

    Initial Commit

---
+++ Test_CI_CD/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test_CI_CD
+//
+//  Created by Muhammad Tohirov on 06/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test_CI_CD/Test_CI_CDApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_CI_CDApp.swift
+//  Test_CI_CD
+//
+//  Created by Muhammad Tohirov on 06/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Test_CI_CDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
