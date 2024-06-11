fea340e00fc9e0b570d4e36b6351e1e0416148b4
Muhammad <muhammad.tohirov@fido.uz>
2024-06-11 10:18:35 +0500

    Greating to the world

--- Test_CI_CD/ContentView.swift
+++ Test_CI_CD/ContentView.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var text = "Hello, XCode Cloud"
+    @State var text = "Hello world, XCode Cloud"
     var body: some View {
         VStack {
             Image(systemName: "globe")
